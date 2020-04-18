19690a595878b9fda3bb8a3dd9e17289bb65ad3f
dev2 <habib@gmail.com>
2020-04-18 18:23:55 +0530

    Test java changed by dev2

--- FirstProDemo/src/com/app/test/Test.java
+++ FirstProDemo/src/com/app/test/Test.java
@@ -1,5 +1,13 @@
 package com.app.test;
 
+import com.app.bean.Utility;
+
 public class Test {
 
+	
+	public static void main(String [] args)
+	{
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+	}
 }
